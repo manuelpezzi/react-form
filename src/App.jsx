fde34c4c03a74b82c572390e1a814c73642ce6b7
manuelpezzi <pezzimanuel@gmail.com>
2025-02-24 13:02:43 +0100

    new article create

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 
+import { useState } from "react"
 
-function App() {
-  const articoli = [
+const App = () => {
+  const [articles, setArticles] = useState([
     "come usare word",
     "come usare excel",
     "come sconffiggere la morte nera"
-  ]
+  ]);
+  const [newArticle, setNewarticle] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newArticle.trim()) {
+      setArticles([...articles, newArticle]);
+      setNewarticle("");
+    }
+  }
+
+
   return (
     <>
       <div>
         <h1>lista articoli </h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text"
+            value={newArticle}
+            onChange={(e) => setNewarticle(e.target.value)} />
+        </form>
         <ul>
-          {articoli.map((title, index) => (
+          {articles.map((title, index) => (
             <li key={index}>{title}</li>
           ))}
         </ul>
