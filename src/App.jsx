62ba850f0fed730146e7cefe821b2a7c0f1289f1
manuelpezzi <pezzimanuel@gmail.com>
2025-02-24 12:47:11 +0100

    lista statica creata

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 
 
 function App() {
-
+  const articoli = [
+    "come usare word",
+    "come usare excel",
+    "come sconffiggere la morte nera"
+  ]
   return (
     <>
-
+      <div>
+        <h1>lista articoli </h1>
+        <ul>
+          {articoli.map((title, index) => (
+            <li key={index}>{title}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
